c99b0eb9e4f74951c2e791c4e09a0298fafa2241
KGrabowski <Kamil.Grabowski@hemmersbach.com>
2018-08-29 15:48:07 +0200

    first component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class App extends React.Component {
+
+    render() {
+        return <div>
+            <i>Hello!!!</i>
+        </div>
+    }
+
+
+
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './index.css';
-
+import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
+import App from "./App";
 
-// ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
