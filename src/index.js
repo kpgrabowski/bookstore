76e769610514d1f7247da2ea9b4aa0d8f51827bb
KGrabowski <Kamil.Grabowski@hemmersbach.com>
2018-08-29 13:43:13 +0200

    Start

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './index.css';
+
+import registerServiceWorker from './registerServiceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
