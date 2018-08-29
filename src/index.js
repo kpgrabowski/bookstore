595bb8dc31fd107319f2f7006acc21b3b0abaac0
Zofia Grabowska <grabowskikamil.kg@gmail.com>
2018-08-29 17:40:47 +0200

    first test

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import './index.css';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
-import App from "./App";
+import App from "./components/App";
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
 registerServiceWorker();
