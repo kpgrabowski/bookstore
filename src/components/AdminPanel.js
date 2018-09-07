806f32e0ca8a268d0ab2c085f9609fb64b4d0ecc
KGrabowski <Kamil.Grabowski@hemmersbach.com>
2018-09-07 15:48:39 +0200

    state in AdminPanel

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -2,33 +2,95 @@ import React from 'react';
 
 class AdminPanel extends React.Component {
 
+    constructor() {
+        super();
+        this.state = {
+            book: {
+                name: "",
+                author: "",
+                description: "",
+                onStock: true,
+                image: ""
+            },
+            books: [],
+
+        };
+    };
+
+    handleChange = (event) => {
+
+
+        let newBook;
+
+        if(event.target.name === "onStock") {
+            newBook = {
+                ...this.state.book,
+                [event.target.name]: event.target.checked
+            };
+        }else {
+            newBook = {
+                ...this.state.book,
+                [event.target.name]: event.target.value
+            };
+        }
+        this.setState({
+            book: newBook
+        });
+    };
+
+    addNewBook =(event) => {
+
+        event.preventDefault();
+
+        let newBooks = [...this.state.books];
+
+        let newBook = {...this.state.book};
+
+        newBooks.push(newBook);
+        this.setState({
+            books: newBooks,
+            book: {
+                name: "",
+                author: "",
+                description: "",
+                onStock: true,
+                image: ""
+            },
+        });
+
+    };
+
+
 
     render() {
 
-
-
-        return (<div className="adminPanel col-4">
-            <form>
-                <div className="form-group">
-                    <input type="text" placeholder="Book name" id="bookName" name="bookName" className="form-control"/>
-                </div>
-                <div className="form-group">
-                    <input type="text" placeholder="Book author" id="bookAuthor" name="bookAuthor" className="form-control"/>
-                </div>
-                <div className="form-group">
-                    <textarea placeholder="Book description" id="bookDescription" name="bookDescription" className="form-control"/>
-                </div>
-                <div className="form-group">
-                    <input type="checkbox" id="bookStock" name="bookStock" className="form-check-input"/>
-                    <label htlmFor="bookStock" className="form-check-label"> On Stock</label>
-                </div>
-                <div className="form-group">
-                    <input type="text" placeholder="Book image" id="bookImage" name="bookImage" className="form-control"/>
-                </div>
-                <button type="submit" className="btn btn-primary" >Add</button>
-            </form>
-        </div>
-        );
+        return (
+            <div className="adminPanel col-4">
+                <form onSubmit={this.addNewBook}>
+                    <div className="form-group">
+                        <input type="text" placeholder="Book name" id="name" name="name" className="form-control"
+                               onChange={this.handleChange} value={this.state.book.name} />
+                    </div>
+                    <div className="form-group">
+                        <input type="text" placeholder="Book author" id="author" name="author" className="form-control"
+                               onChange={this.handleChange} value={this.state.book.author}/>
+                    </div>
+                    <div className="form-group">
+                        <textarea placeholder="Book description" id="description" name="description" className="form-control"
+                                  onChange={this.handleChange} value={this.state.book.description}/>
+                    </div>
+                    <div className="form-group bookStock">
+                        <input type="checkbox" id="onStock" name="onStock" className="form-check-input"
+                               onChange={this.handleChange} value={this.state.book.onStock}/>
+                        <label htmlFor="onStock" className="form-check-label">On stock</label>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" placeholder="Book image" id="image" name="image" className="form-control"
+                               onChange={this.handleChange} value={this.state.book.image}/>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Add</button>
+                </form>
+            </div>);
     }
 }
 
